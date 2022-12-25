b7d03da1b68b320877ce8289db7fac243fd0b838
kucukkanat <htolgasahin@gmail.com>
2022-12-26 00:35:14 +0200

    add action

---
+++ publisher.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PRIVATE_REPO=https://github.com/kucukkanat/digital-garden.git
+SITE_REPO=https://github.com/kucukkanat/kucukkanat.github.io.git
+
+# Clone private repo
+git clone $PRIVATE_REPO
